cea592aac2bcd019132e81c64094ccfece3554a8
Rizky17 <Rizky17@Rizky>
2018-08-28 18:01:06 +0700

    uji

---
+++ ujitoko.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author Rizky17
+ */
+public class ujitoko {
+    public static void main(String[] args) 
+    {
+        System.out.println("Data Pembelian");
+        System.out.println();
+        System.out.println("----------------------------------");
+        Transaksi dd = new Transaksi(123,"Muhammad AL-Fatih","Istanbul No.3",03213345,122,"Cara memperbanyak pahala"
+                ,50000,"qod kafani",50000);
+        dd.info();
+                
+    }
+}
